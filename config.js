c0075ebfbe8a94f10b2d913297854a5777a4e74f
hamamhany <famtmabatata@gmail.com>
2026-05-07 22:55:02 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  url: 'https://jaxgopgtgjaqecgbajbm.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpheGdvcGd0Z2phcWVjZ2JhamJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5OTAwMzMsImV4cCI6MjA5MzU2NjAzM30.BUCe_u9onFWZQ1NyWrmdEja7H3URSBYi_zM74zD6QIE'
+};
