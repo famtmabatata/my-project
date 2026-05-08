a94d536140fac7d13bf2bdc044542b36d9344209
hamamhany <famtmabatata@gmail.com>
2026-05-08 17:37:58 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = window.APP_CONFIG?.url;
-const SUPABASE_ANON_KEY = window.APP_CONFIG?.key;
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-export { supabase };
